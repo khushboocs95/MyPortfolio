6829bee9455b9a35823835e3aaa0ac89280a9009
khushboocs95 <khushboocs95@gmail.com>
2024-06-14 17:50:50 +0530

    cssCahnges update

--- app.js
+++ app.js
@@ -15,3 +15,60 @@ Cross.addEventListener("click", function () {
   MobileMenu.style.display = "none";
   Webapp.style.display = "block";
 });
+
+// making a tab section for portfoli 
+
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
+
+
+
+
+
+
+// Create a smooth scroller 
+document.addEventListener('DOMContentLoaded', () => {
+  gsap.registerPlugin(ScrollTrigger);
+
+
+  // Example ScrollTrigger animation
+  gsap.to('.banner', {
+      scrollTrigger: {
+          trigger: '.container-full',
+          start: 'top center',
+          end: 'bottom center',
+          scrub: true
+      },
+      scale: 0.8,
+      opacity: 0
+  });
+});
+
+
+// gsap.registerPlugin(ScrollTrigger);
+// let tl = gsap.timeline()
+// tl.from(["#text-1"],{xPercent: -100, y: 500} )
+// .from(["#text-2"],{xPercent: 100, y: 500}, 0 )
+
+//  ScrollTrigger.create({
+//     trigger: ".wrapper",
+//     start: "top 30%",
+//     end: "90% 30%",
+//     scrub: 1,
+//     pin: true,
+//   //  markers: true,
+//     animation: tl.play()
+//   })
